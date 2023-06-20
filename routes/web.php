d3ccd397815be0af085e2feda7f4f5ef450120f7
Federico Airoldi <airoldibusiness@gmail.com>
2023-06-20 02:04:18 +0200

    FASE 1 - creo il nuovo controller e sistemo la route sotto auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +20,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+// Tutte rotte autenticate
+Route::middleware('auth', 'verified')->prefix('admin')->name('admin.')->group(function () {
+
+    // Rotte per la gestione form profilo
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rotte per admin
+    Route::get('/', [DashboardController::class, 'index'] )->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
